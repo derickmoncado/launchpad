298b5999ac3fe58a8c866ef0aa862e6791422281
Derick Moncado <derickmoncado@gmail.com>
2021-09-17 18:08:14 -0500

    Implemented Slick successfully with it's theme/fonts/etc.

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let pathname = window.location.pathname; // Gets URL path and stores in pathname
+
+$(document).ready(function() {
+	console.log('document ready!');
+
+	// For appending the 'active' class to nav
+	$('.navbar-nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+	$('.dropdown-menu > li > a[href="'+pathname+'"]').addClass('active');
+
+	// Initiate Slick
+	$('.slider-example').slick({
+		dots: true,
+		mobileFirst: true,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
+});
