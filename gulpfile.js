0a445279acab0875ce1e848e5f6f1a88ce6a3b59
Derick Moncado <derickmoncado@gmail.com>
2021-09-20 19:10:05 -0500

    Working on first gulp task to compile SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+'use strict';
+const {src, dest, watch, series, parallel } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer  = require('gulp-autoprefixer');
+
+
+// Compile SCSS into CSS
+function compileSCSS() {
+  console.log('----COMPILING SCSS!----');
+  return src('src/assets/scss/main.scss')
+	.pipe(sass({
+	  outputStyle: 'expanded',
+	  sourceComments: 'map',
+	  sourceMap: 'scss',
+	}).on('error', sass.logError))
+	.pipe(autoprefixer('last 2 versions'))
+	.pipe(sourcemaps.write())
+	.pipe(dest('dist/assets/css'));
+	//.pipe(browserSync.stream());
 }
 
-exports.default = defaultTask
+exports.compileSCSS = compileSCSS;
