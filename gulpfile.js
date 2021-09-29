1da1563491677639d2c8840dfe0c7b0c8754cd03
Derick Moncado <derickmoncado@gmail.com>
2021-09-28 20:47:14 -0500

    Successfully added new gulp task to compile AND watch files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 'use strict';
-const {src, dest, watch, series, parallel } = require('gulp');
+const {src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer  = require('gulp-autoprefixer');
 
 
-// Compile SCSS into CSS
+// compile SCSS into CSS
 function compileSCSS() {
   console.log('----COMPILING SCSS!----');
   return src('src/assets/scss/main.scss')
@@ -16,8 +16,15 @@ function compileSCSS() {
 	}).on('error', sass.logError))
 	.pipe(autoprefixer('last 2 versions'))
 	.pipe(sourcemaps.write())
-	.pipe(dest('dist/assets/css'));
+	.pipe(dest('css'));
 	//.pipe(browserSync.stream());
 }
 
-exports.compileSCSS = compileSCSS;
+// watch files
+function watchFiles() {
+  watch(['src/assets/scss/**/*.scss', 'src/assets/scss/*.scss'] , compileSCSS);
+  // watch('src/assets/js/*.js', compileJS); let not worry about this yet
+}
+
+// gulp dev
+exports.dev = series(compileSCSS, watchFiles);
