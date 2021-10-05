61ce2998889d078d40cc8c7faaf279ef38f57f94
Derick Moncado <derickmoncado@gmail.com>
2021-10-04 19:06:08 -0500

    Working on compile html with all other assets tast

--- gulpfile.js
+++ gulpfile.js
@@ -21,21 +21,30 @@ function compileSCSS() {
 	.pipe(browserSync.stream());
 }
 
+// USING PANINI, TEMPLATE, PAGE AND PARTIAL FILES ARE COMBINED TO FORM HTML MARKUP
+function compileHTML() {
+  console.log('----COMPILING HTML----');
+  return src('./*.html')
+	.pipe(dest('./'))
+	.pipe(browserSync.stream());
+}
+
 // browserSync
 function browserSyncInit(done) {
   console.log('----BROWSER SYNC----');
   browserSync.init({
 	notify: false,
-	server: './'
+	server: '' // THIS NEEDS TO BE SOMETHING LIKE DIST AND ALL THE CSS NEEDS TO GO THERE TOO
   });
   return done();
 }
 
 // watch files
 function watchFiles() {
-  watch(['src/assets/scss/**/*.scss', 'src/assets/scss/*.scss'] , compileSCSS);
+  watch('./*.html', compileHTML);
+  watch(['src/assets/scss/**/*.scss', 'src/assets/scss/*.scss'], compileSCSS);
   // watch('src/assets/js/*.js', compileJS); let not worry about this yet
 }
 
 // gulp dev
-exports.dev = series(compileSCSS, browserSyncInit, watchFiles);
+exports.dev = series(compileHTML, compileSCSS, browserSyncInit, watchFiles);
