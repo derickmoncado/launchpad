66e54bdbe305050798a7d16bd1484bb6572ab180
Derick Moncado <derickmoncado@gmail.com>
2021-11-27 15:46:41 -0800

    Nuked jQuery. RIP. Gone but never forgotten.

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -2,32 +2,34 @@
 
 let pathname = window.location.pathname; // Gets URL path and stores in pathname
 
-$(document).ready(function() {
-	console.log('document ready!');
-
+//$(document).ready(function() {
+	//console.log('document ready!');
 	// For appending the 'active' class to nav
-	$('.navbar-nav > li > a[href="'+pathname+'"]').parent().addClass('active');
-	$('.dropdown-menu > li > a[href="'+pathname+'"]').addClass('active');
+	//$('.navbar-nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+	//$('.dropdown-menu > li > a[href="'+pathname+'"]').addClass('active');
+//});
 
-	// Init Swiper
+
+
+
+const init = () => {
+	console.log('document ready!');
+	
+	// Swiper
 	const swiper = new Swiper('.swiper', {
 		loop: true,
-
-		// If we need pagination
 		pagination: {
 			el: '.swiper-pagination',
 		},
-	
-		// Navigation arrows
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
 		},
-	
-		// And if we need scrollbar
 		scrollbar: {
 			el: '.swiper-scrollbar',
 		},
 	});
 
-});
+};
+
+init();
