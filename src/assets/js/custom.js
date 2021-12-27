22d1b51ed5e8f495ed05ebe8642c76302a03820e
Derick Moncado <derickmoncado@gmail.com>
2021-12-26 16:50:51 -0800

    Adding content to how-to-use page

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -9,7 +9,7 @@
 		el.classList.add('active');
 	});
 
-  // Init Swiper
+  // Init Swiper (swiper.js)
 	const swiper = new Swiper('.swiper', {
 		loop: true,
 		pagination: {
@@ -24,12 +24,12 @@
 		},
 	});
 
-	// Init Emergence
+	// Init Emergence (emergence.js)
 	emergence.init();
 
 	// Init tooltips everywhere (popper.js)
-	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-		return new bootstrap.Tooltip(tooltipTriggerEl)
-	});
+	var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+	var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+		return new bootstrap.Popover(popoverTriggerEl)
+	})
 })();
